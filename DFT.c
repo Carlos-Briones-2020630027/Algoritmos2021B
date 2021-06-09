29ca21f3a7999616d6adb14749841e1e898d565d
Carlos-Briones-2020630027 <85136394+Carlos-Briones-2020630027@users.noreply.github.com>
2021-06-08 19:14:17 -0500

    Update DFT.c

--- DFT.c
+++ DFT.c
@@ -10,21 +10,18 @@
 /*Intentando aplicar DFT en {1, 2, 3, 4, 5, 6, 7}*/
 
 int main(){
-    float re[size], ima[size], x[size];
+    float re[size], ima[size];
     int arrayN[size]={1,2,3,4,5,6,7};
-    srand(time(0));
-    for(int i=0; i<size;i++){
-        x[i]=(2.0*rand()/RAND_MAX)-1.0;
-    }
+   
 
     for(int k=0; k<size; k++){
         for(int i=0;i<size;i++){
             re[k]=0;
-            re[k] += x[i]*cos((PIx2*i*k)/size);   
+            re[k] += arrayN[i]*cos((PIx2*i*k)/size);   
         }
         for(int i=0;i<size;i++){
             ima[k]=0;
-            ima[k] -= x[i]*sin((PIx2*i*k)/size);
+            ima[k] -= arrayN[i]*sin((PIx2*i*k)/size);
         }
     }
     printf("Reales\n");
